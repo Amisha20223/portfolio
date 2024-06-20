0f9a88e80f2398b6d00e37338bbb9d0b196f8f18
Amisha20223 <amisha.2125ec1132@kiet.edu>
2024-06-20 10:51:55 +0530

    navbar

--- components/main/hero.tsx
+++ components/main/hero.tsx
@@ -1,19 +1,20 @@
-import React from 'react'
+import React from "react";
+import HeroContent from "../sub/HeroContent";
 
-const hero = () => {
+const Hero = () => {
   return (
-    <div className='relative flex flex-col h-full w-full'>
-        <video 
+    <div className="relative flex flex-col h-full w-full" id="about-me">
+      <video
         autoPlay
         muted
         loop
-        className='rotate-180 absolute top-[-340px] left-0 z-[1] w-full h-full object-cover'
-        >
-            <source  src='/blackhole.webm' type='video/webm' />
-
-        </video>
+        className="rotate-180 absolute top-[-340px]  h-full w-full left-0 z-[1] object-cover "
+      >
+        <source src="/blackhole.webm" type="video/webm" />
+      </video>
+      <HeroContent />
     </div>
-  )
-}
+  );
+};
 
-export default hero
+export default Hero;
