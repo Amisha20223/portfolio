dc28f7c17a82056214c225d692a2c8b12d63ee38
Amisha20223 <120022430+Amisha20223@users.noreply.github.com>
2024-06-30 14:11:40 +0530

    Update Encryption.tsx

--- components/main/Encryption.tsx
+++ components/main/Encryption.tsx
@@ -13,7 +13,7 @@ const Encryption = () => {
           variants={slideInFromTop}
           className="text-[40px] font-medium text-center text-gray-200"
         >
-          Performance
+          Performance 
           <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
             {" "}
             &{" "}
