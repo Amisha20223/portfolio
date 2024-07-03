d004f57b3204f680f2729fb7877c36a7bfbabd28
Amisha20223 <amisha.2125ec1132@kiet.edu>
2024-07-03 23:45:28 +0530

    new

--- src/components/main/Footer.tsx
+++ src/components/main/Footer.tsx
@@ -64,7 +64,7 @@ const Footer = () => {
             </div>
 
             <div className="mb-[20px] text-[15px] text-center">
-                &copy; WebChain Dev 2023 Inc. All rights reserved
+                &copy; 
             </div>
         </div>
     </div>
