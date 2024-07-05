76cc67f7bd7709d063d4248a656c530f3c7638dc
Amisha20223 <120022430+Amisha20223@users.noreply.github.com>
2024-07-05 14:24:44 +0530

    Update Projects.tsx

--- src/components/main/Projects.tsx
+++ src/components/main/Projects.tsx
@@ -30,15 +30,7 @@ planning.
 • Designed and implemented user-friendly interfaces for efficient billing and invoicing.
 • Integrated real-time data updates and responsive design to enhance user experience"
         />
-{/*         <ProjectCard
-          src="/porti.png"
-          title="Portfolio"
-          description=""
-        />
-         <ProjectCard
-          src="/foodi.png"
-          title="OrderMeal"
-          description="" */}
+
         />
       </div>
     </div>
